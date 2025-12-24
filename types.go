bcb44ca058d77e79cb7513536ca53616f13ab303
Waleed Ahmad <137788903+Waleed-Ahmad-dev@users.noreply.github.com>
2025-12-24 14:27:08 +0500

    Update types.go

--- types.go
+++ types.go
@@ -2,7 +2,8 @@ package main
 
 import "time"
 
-// FileInfo defines the JSON structure for our API
+// ... (Keep existing FileInfo, ConfigFile, CreateDirRequest) ...
+
 type FileInfo struct {
 	Name    string `json:"name"`
 	Size    int64  `json:"size"`
@@ -11,18 +12,18 @@ type FileInfo struct {
 	Type    string `json:"type"`
 }
 
-// ConfigFile is what we save to disk (gofiles.json)
 type ConfigFile struct {
 	Username  string    `json:"username"`
-	Password  string    `json:"password"` // In production, we should hash this!
+	Password  string    `json:"password"`
 	CreatedAt time.Time `json:"created_at"`
 }
 
-// ... (Keep CreateDirRequest, ActionRequest, TrashInfo as they were) ...
 type CreateDirRequest struct {
 	Path string `json:"path"`
 	Name string `json:"name"`
 }
+
+// ... (Keep ActionRequest, TrashInfo) ...
 
 type ActionRequest struct {
 	SourcePath string `json:"sourcePath"`
@@ -34,4 +35,11 @@ type TrashInfo struct {
 	OriginalPath string    `json:"originalPath"`
 	DeletedAt    time.Time `json:"deletedAt"`
 	Filename     string    `json:"filename"`
+}
+
+// NEW: For Zip/Unzip operations
+type ArchiveRequest struct {
+	SourcePath string `json:"sourcePath"` // File/Folder to zip, or Zip file to unzip
+	DestPath   string `json:"destPath"`   // Where to save
+	Password   string `json:"password"`   // Optional: Leave empty for no password
 }
