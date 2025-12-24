3ae72ee1be7491678df86f7a3e19d3a2ae7d0273
Waleed Ahmad <137788903+Waleed-Ahmad-dev@users.noreply.github.com>
2025-12-24 12:54:48 +0500

    Update config.go

--- config.go
+++ config.go
@@ -1,5 +1,9 @@
 package main
 
+import "time"
+
 // Config: The root folder we are browsing
 // "." means the current folder where the program is running
 const RootFolder = "."
+const TrashFolder = ".trash"
+const TrashRetention = 30 * 24 * time.Hour // 30 Days
